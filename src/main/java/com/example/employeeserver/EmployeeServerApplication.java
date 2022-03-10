6d26e6ea8d18a9288388715fad2496d748cbb349
AKASH ANAND <68923458+AKASHANAND8029@users.noreply.github.com>
2022-03-10 16:58:11 +0530

    2commit

--- src/main/java/com/example/employeeserver/EmployeeServerApplication.java
+++ src/main/java/com/example/employeeserver/EmployeeServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+//This is server application
 public class EmployeeServerApplication {
 
     public static void main(String[] args) {
